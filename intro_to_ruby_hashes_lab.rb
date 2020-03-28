4d0a2ed77d4d785a8c769958fdfacf5d09e4b066
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-03-28 19:38:28 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -53,12 +53,12 @@ def monopoly_with_third_tier
           },
           :b_and_o_railroad => {
 
-          }, 
+          },
           :shortline_railroad => {
-            
-          } 
+
+          }
           
-    
+
 
         }
     },
